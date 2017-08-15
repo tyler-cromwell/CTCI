0f1804e83c29d8792dd481c1c53878b904137a96
Tyler Cromwell <tjc6185@gmail.com>
2017-08-14 20:14:18 -0400

    Changed output and added error checking

--- Chapter_01/1.1-Is_Unique.py
+++ Chapter_01/1.1-Is_Unique.py
@@ -4,11 +4,17 @@ import sys
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Please enter a string on the command line')
+        sys.exit(1)
+    else:
+        print(''.join(['String \"', sys.argv[1], '\" is unique: ']), end='')
+
     s = ''.join(sorted(sys.argv[1]))
 
     for i in range(1, len(s)):
         if s[i] == s[i-1]:
-            print(''.join(['String \"', sys.argv[1], '\" is NOT unique']))
+            print('false')
             sys.exit(1)
 
-    print(''.join(['String \"', sys.argv[1], '\" is unique']))
+    print('true')
