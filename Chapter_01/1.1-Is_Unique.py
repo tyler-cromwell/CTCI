152d8715fb8215f89f2bac2256dcf7d1b988f89f
Tyler Cromwell <tjc6185@gmail.com>
2017-08-14 20:30:26 -0400

    Fixed description

--- Chapter_01/1.1-Is_Unique.py
+++ Chapter_01/1.1-Is_Unique.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Solution to question 1.1 - Is Unique.
+Solution to question 1.1 - Is Unique
 Assumptions:
-  - ASCII strings.
+  - Strings are ASCII
 """
 
 import sys
