fd401f38d57efe60627f6da19809ac62308755a5
Tyler Cromwell <tjc6185@gmail.com>
2017-08-19 21:53:17 -0400

    Added C 11 solution to question 1.4

---
+++ Chapter_01/1.4-Palindrome-Permutation.c
@@ -0,0 +1,88 @@
+/*************************************************
+ * Solution to question 1.4 - Palindrome Permutation
+ * Assumptions:
+ *   - Strings are ASCII
+ *   - Lowercase only
+ *
+ * Notes:
+ *   - Algorithm will only handle alphabetic strings
+*************************************************/
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ALPHABET_LENGTH 26
+
+
+int compare(const void *a, const void *b) {
+    const char *aa = a;
+    const char *bb = b;
+    return strcmp(aa, bb);
+}
+
+
+bool isstring(const char *s) {
+    for (size_t i = 0; i < strlen(s); i++) {
+        if (isprint(s[i]) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2 || !isstring(argv[1])) {
+        fprintf(stderr, "Please enter a string on the command line\n");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "String \"%s\" is a palindrome permutation: ", argv[1]);
+
+    long ones = 0;
+    size_t l = strlen(argv[1]);
+    long *counts = calloc(ALPHABET_LENGTH, sizeof(long));
+
+    for (size_t i = 0; i < l; i++) {
+        if (argv[1][i] == ' ') {
+            l--;
+            continue;
+        }
+        else if (argv[1][i] >= 'a' && argv[1][i] <= 'z') {
+            counts[argv[1][i] - 'a']++;
+        }
+        else {
+            fprintf(stdout, "false\n");
+            goto exit;
+        }
+    }
+
+    if (l % 2 == 0) {
+        for (size_t i = 0; i < ALPHABET_LENGTH; i++) {
+            if (counts[i] == 1) {
+                fprintf(stdout, "false\n");
+                goto exit;
+            }
+        }
+    }
+    else {
+        for (size_t i = 0; i < ALPHABET_LENGTH; i++) {
+            if (counts[i] == 1) {
+                ones++;
+            }
+            if (ones > 1) {
+                fprintf(stdout, "false\n");
+                goto exit;
+            }
+        }
+    }
+
+    fprintf(stdout, "true\n");
+exit:
+    free(counts);
+    return EXIT_SUCCESS;
+}
