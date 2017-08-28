399e4a6c06848f8de09353225c9246324d6cc43a
Tyler Cromwell <tjc6185@gmail.com>
2017-08-27 22:51:20 -0400

    Added common header file

---
+++ Chapter_01-Arrays_and_Strings/common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdbool.h>
+
+extern int ascending(const void *a, const void *b);
+extern bool isinteger(const char *s);
+extern bool isstring(const char *s);
+
+#endif
