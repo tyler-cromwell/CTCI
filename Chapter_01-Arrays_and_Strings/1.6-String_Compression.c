4d9504206a886351557505b3613b8926dcb26bdd
Tyler Cromwell <tjc6185@gmail.com>
2017-09-03 18:02:10 -0400

    Completed C 11 solution to question 1.6

--- Chapter_01-Arrays_and_Strings/1.6-String_Compression.c
+++ Chapter_01-Arrays_and_Strings/1.6-String_Compression.c
@@ -1,17 +1,15 @@
 /*************************************************
  * Solution to question 1.6 - String Compression
  *
- * Status: INCOMPLETE
+ * Status: COMPLETE
  *
  * Assumptions:
  *   - Strings are ASCII
  *   - Strings are alphabetical only
  *   - Case insensitive
- *
- * TODO:
- *   - Handle counts > 9
 *************************************************/
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,9 +17,26 @@
 #include "common.h"
 
 
+static inline size_t ldigits(long number) {
+    return (size_t) (floor(log10(labs(number))) + 1);
+}
+
+
+static long lreverse(long num) {
+    long rev = 0;
+
+    while (num > 0) {
+        rev = (rev * 10) + (num % 10);
+        num /= 10;
+    }
+
+    return rev;
+}
+
+
 int main(int argc, char *argv[]) {
     if (argc < 2 || !isstring(argv[1])) {
-        fprintf(stderr, "Please enter two strings on the command line\n");
+        fprintf(stderr, "Please enter a string on the command line\n");
         return EXIT_FAILURE;
     }
 
@@ -32,37 +47,48 @@ int main(int argc, char *argv[]) {
         .length = strlen(argv[1])
     };
 
-    // Determine how many char/count pairs we'll need
-    size_t pairs = 0;
+    // Determine how many chars pairs we'll need
+    size_t chars = 0, digits = 0, count = 1;
 
     for (size_t i = 1; i <= s1.length; i++) {
         if (s1.data[i] != s1.data[i-1]) {
-            pairs++;
+            chars++;
+            digits += ldigits(count);
+            count = 1;
+        } else {
+            count++;
         }
     }
 
     // If new string is not smaller than original
-    if (pairs * 2 >= s1.length) {
+    if (chars + digits >= s1.length) {
         fprintf(stdout, "%s\n", s1.data);
         return EXIT_SUCCESS;
     }
 
     // Allocate space for the pairs in a new string
     struct str s2 = {
-        .data = calloc((pairs * 2) + 1, sizeof(char)),
-        .length = pairs * 2
+        .data = calloc((chars + digits) + 1, sizeof(char)),
+        .length = chars + digits
     };
 
-    unsigned char count = 1;
-    size_t j = 0;
+    size_t s = 0;
 
-    // Get the counts
+    // Create new string
     for (size_t i = 1; i <= s1.length; i++) {
         if (s1.data[i] != s1.data[i-1]) {
-            s2.data[j+0] = s1.data[i-1];    // Add the character
-            s2.data[j+1] = count + '0';     // Add its count
+            s2.data[s] = s1.data[i-1];    // Add the character
+            size_t reverse = lreverse(count);
+
+            // Save each digit
+            for (size_t j = 0; j < ldigits(count); j++) {
+                s++;
+                s2.data[s] = (reverse % 10) + '0';
+                reverse /= 10;
+            }
+
             count = 1;
-            j += 2;
+            s++;
         } else {
             count++;
         }
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ My solutions to the programming questions in Cracking the Coding Interview (6th 
 # Solutions
 | Chapter | Completed | Language |                         Reason                       |
 |:-------:|:---------:|:--------:|:----------------------------------------------------:|
-|    1    |    5/9    |     C    | Strings are character arrays by default              |
+|    1    |    6/9    |     C    | Strings are character arrays by default              |
 |    2    |    0/8    |    C++   | New language for me, has more data structures than C |
