a8be6c08589f1fff1e130a996fc48a0b23a04ecc
Tyler Cromwell <tjc6185@gmail.com>
2017-09-13 11:54:57 -0400

    Started Java 1.8 solution to question 7.3

---
+++ Chapter_07-Object-Oriented_Design/Song.java
@@ -0,0 +1,37 @@
+/*************************************************
+ * Part of solution to question 7.3 - Jukebox
+*************************************************/
+
+public class Song {
+    private String title;
+    private String artist;
+    private String album;
+    private long length;    // seconds
+
+    public Song(String title, String artist, String album, long length) {
+        this.title = title;
+        this.artist = album;
+        this.album = album;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public long getLength() {
+        return this.length;
+    }
+
+    public String getTime() {
+        return ((long) (this.length / 60)) +":"+ String.format("%02d", this.length % 60);
+    }
+}
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ My solutions to the programming questions in Cracking the Coding Interview (6th 
 | Chapter | Completed | Language |                         Reason(s)                         |
 |:-------:|:---------:|:--------:|:---------------------------------------------------------:|
 |    1    |    6/9    |     C    | Strings are character arrays by default, mixing languages |
+|    7    |    0/12   |    Java  | Object-oriented by nature, mixing languages               |
 |    8    |    1/14   |   Python | High-level, powerful, mixing languages                    |
