a70c38e107473ae7b514afb3b378d70ac302e51c
Tyler Cromwell <tjc6185@gmail.com>
2017-10-11 11:48:31 -0400

    Working on data structures file for Python 3 solution to question 8.9

---
+++ Chapter_08-Recursion_and_Dynamic_Programming/Question_8.9/structs.py
@@ -0,0 +1,48 @@
+##################################################
+#  Part of solution to question 8.9 - Parens
+# 
+#  Status: INCOMPLETE
+##################################################
+
+
+class DNode:
+    def __init__(self, data):
+        self.__prev = None
+        self.__data = data
+        self.__next = None
+
+    def getPrev(self):
+        return self.__prev
+
+    def getData(self):
+        return self.__data
+
+    def getNext(self):
+        return self.__next
+
+    def setPrev(self, node):
+        self.__prev = node
+
+    def setNext(self, node):
+        self.__next = node
+
+
+class Stack:
+    def __init__(self, data):
+        self.__base = DNode(data)
+        self.__top = self.__base
+        self.__size = 1
+
+    def __len__(self):
+        return self.__size
+
+    def push(self, node):
+        node.setPrev(self.__top)    # Link to original top
+        self.__top.setNext(node)    # Link from original top
+        self.__top = node           # Set new top
+        self.__size += 1            # Increase stack size
+
+    def pop(self):
+        self.__top = self.__top.getPrev()   # Set top to node before top
+        self.__top.setNext(None)            # Unlink original top
+        self.__size -= 1                    # Decrease stack size
