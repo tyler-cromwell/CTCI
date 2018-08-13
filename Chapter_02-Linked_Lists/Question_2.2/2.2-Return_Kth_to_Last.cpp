a95e00c036f4b2037e4e20b20bf8f813f4a28047
Tyler Cromwell <tjc6185@gmail.com>
2018-08-12 22:28:36 -0400

    Added C++ 11 solution to question 2.2

---
+++ Chapter_02-Linked_Lists/Question_2.2/2.2-Return_Kth_to_Last.cpp
@@ -0,0 +1,65 @@
+/*************************************************
+ * Solution to question 2.2 - Return Kth to Last
+ *
+ * Status: INCOMPLETE
+*************************************************/
+
+#include <iostream>
+
+#include "../snode.h"
+#include "../slist.h"
+
+using namespace std;
+using namespace ctci;
+
+
+class SListSub : public SList {
+    public:
+        SListSub(int data);
+        ~SListSub();
+
+        SNode * kth_to_last(size_t k);
+};
+
+
+SListSub::SListSub(int data) : SList(data) {}
+
+SListSub::~SListSub() {}
+
+
+SNode * SListSub::kth_to_last(size_t k) {
+    SNode *target = this->head;
+
+    for (size_t i = 0; i < (this->size - k); i++) {
+        target = target->getNext();
+    }
+
+    return target;
+}
+
+
+void print_slist(ostream &file, SListSub &slist) {
+    SNode *ptr = slist.getHead();
+
+    do {
+        file << ptr->getData() << endl;
+        ptr = ptr->getNext();
+    }
+    while (ptr != NULL);
+}
+
+
+int main(void) {
+    SListSub slist(7);
+    slist.add(4);
+    slist.add(2);   // Target
+    slist.add(1);   // 2nd to last
+    slist.add(5);
+
+    size_t k = 3;
+    SNode *node = slist.kth_to_last(k);
+    cout << "Retrieving k = " << k << " from last node" << endl;
+    cout << node->getData() << endl;
+
+    return EXIT_SUCCESS;
+}
